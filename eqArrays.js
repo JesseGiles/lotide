80284ff0c772ecca8d4f627171d9bcc7c1c52db1
Jesse Giles <jesse.giles@telus.com>
2022-11-15 18:33:30 -0700

    corrected output on eqarrays to truthy/false not assertequal messaging

--- eqArrays.js
+++ eqArrays.js
@@ -31,13 +31,7 @@ const eqArrays = function(array1, array2) {
   } return true;
 };
 
-//eqArrays([1, 2, 3], [1, 2, 3]) // => true
-//eqArrays([1, 2, 3], [3, 2, 1]) // => false
-
-//eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-//eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => should FAIL
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => should FAIL
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true  < ----  calling 
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false  <---- logging
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => should PASS
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => should FAIL
