85809f569500f4b0db4143945532f2f13f1c5a77
Jesse Giles <jesse.giles@telus.com>
2022-11-17 16:04:41 -0700

    linted map.js

--- map.js
+++ map.js
@@ -28,7 +28,7 @@ const assertArrayEqual = function(input1, input2) {
 const words = ["ground", "control", "to", "major", "tom"];
 const numbers = [1, 2, 3, 4, 5];
 const mixedbag = ["hockey", 39, "karate", 152, [1, 2, 3]];
-const failtest = "this should fail"
+const failtest = "this should fail";
 
 const map = function(array, callback) {
   const results = [];
@@ -37,22 +37,22 @@ const map = function(array, callback) {
     results.push(callback(item));
   }
   return results;
-}
+};
 
 const results1 = map(words, word => word[0]);
 const results2 = map(numbers, nums => nums + nums);
 const results3 = map(mixedbag, values => values[1]);
-const results4 = map(failtest, string => string)
+const results4 = map(failtest, string => string);
 
 
 console.log(results1);
 assertArrayEqual(results1, ['g', 'c', 't', 'm', 't']);
 
 console.log(results2);
-assertArrayEqual(results2, [2, 4, 6, 8, 10])
+assertArrayEqual(results2, [2, 4, 6, 8, 10]);
 
 console.log(results3);
-assertArrayEqual(results3, ["o", 9, "a", 2])
+assertArrayEqual(results3, ["o", 9, "a", 2]);
 
 console.log(results4);
 assertArrayEqual(results4, "this should fail");
