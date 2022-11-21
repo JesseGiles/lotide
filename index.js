d40642ec3cac78d0be6a697d5ab8911761a2f053
Jesse Giles <jesse.giles@telus.com>
2022-11-21 14:12:11 -0700

    created index.js and added require for all prev created functions

---
+++ index.js
@@ -0,0 +1,10 @@
+// index.js
+const head   = require('./head');
+const tail   = require('./tails');
+const middle = require('./middle');
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle
+};
