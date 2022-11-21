0bde7925c7fb18984b952cc101c10209e50cd9d8
Jesse Giles <jesse.giles@telus.com>
2022-11-21 14:06:03 -0700

    updated tail and middle tests to utilize chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,11 +1,22 @@
 // test/middle.js
 
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+
 // TEST CODE
 // ...
 
-assertArraysEqual(middle([1, 2]), [])
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([5, 10, 15, 20, 25, 30, 35, 40, 45]), [25]);
+describe("#middle", () => {
+  it("returns empty array if there are < 2 items in the array", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+
+  it("should return the middle element on an array with odd # of values", () => {
+    assert.deepEqual(middle([5, 10, 15, 20, 25, 30, 35, 40, 45]), [25]);
+  });
+
+  it("should return 2 middle elements on an array with even # of values", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,20 +1,23 @@
 // test/tailTest.js
-
+const assert = require('chai').assert;
 const tail = require('../tails');
-const assertEqual = require('../assertEqual');
+//const assertEqual = require('../assertEqual');
 // TEST CODE
 // ...
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back 2 elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
 
-const words = ["yo yo", "lighthouse", "labs"];
-tail(words);
-assertEqual(words.length, 3); // original array should still have 3 elements
+  it("array with one element should return empty array", () => {
+    assert.deepEqual(tail([5]), []);
+  });
 
-assertEqual(tail([5]), []); // array with one element should return empty array
-//console.log(tail([5]), 5); // confirm empty array returned
-assertEqual(tail([]), []); // empty array should yield empty array
-//console.log(tail([]), 5); // confirm empty array returned
+  it("empty array should yield empty array", () => {
+    assert.deepEqual(tail([]), []);
+  });
+
+});
+
+
